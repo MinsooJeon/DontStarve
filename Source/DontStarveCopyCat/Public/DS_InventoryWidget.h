5ef767a3d18e90307065c10d7ef75afdfe274e4b
MinsooJeon <88232069+MinsooJeon@users.noreply.github.com>
2025-04-29 18:46:53 +0900

    20250429 인게임 UI 틀 배치중.

---
+++ Source/DontStarveCopyCat/Public/DS_InventoryWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DS_InventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DONTSTARVECOPYCAT_API UDS_InventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DontStarveCopyCat/Public/DS_StatWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DS_StatWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DONTSTARVECOPYCAT_API UDS_StatWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
